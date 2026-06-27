f995d4448ba036ff7be340ff479e27d2b5c8d1fb
Lahari Gandla(UST <304579@ust.com>
2026-06-27 17:48:41 +0530

    pact

--- Week4_Gate4/ProviderConsumerPact/src/test/java/com/ust/sdet/pact/OmsProviderPactTest.java
+++ Week4_Gate4/ProviderConsumerPact/src/test/java/com/ust/sdet/pact/OmsProviderPactTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 @PactBroker(
         url = "http://127.0.0.1:9292"
 )
-//@PactFolder("target/pacts")
+//@PactFolder("target/pacts") // to run in local
 public class OmsProviderPactTest {
     @RegisterExtension
     private static final WireMockExtension wireMock =
