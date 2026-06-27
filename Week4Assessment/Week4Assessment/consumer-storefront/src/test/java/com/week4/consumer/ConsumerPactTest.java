0d53597d2e4bdfcd9108fc099f5429be58c6072c
Jyothsna Vaidyanath(UST <307395@ust.com>
2026-06-27 18:10:28 +0530

    THE final commit for documentation

--- Week4Assessment/Week4Assessment/consumer-storefront/src/test/java/com/week4/consumer/ConsumerPactTest.java
+++ Week4Assessment/Week4Assessment/consumer-storefront/src/test/java/com/week4/consumer/ConsumerPactTest.java
@@ -2,8 +2,10 @@ package com.week4.consumer;
 
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactBuilder;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import org.junit.jupiter.api.Test;
@@ -52,5 +54,37 @@ public class ConsumerPactTest {
         int statusCode = connection.getResponseCode();
 
         assertEquals(200, statusCode);
+    }
+
+    @Pact(consumer = "storefront-web", provider = "catalog-service")
+    public V4Pact skuMissing(PactDslWithProvider builder) {
+
+        return builder
+                .given("SKU missing")
+                .uponReceiving("GET missing SKU")
+                .path("/catalog/SKU-999")
+                .method("GET")
+                .willRespondWith()
+                .status(404)
+                .body("""
+                {
+                  "message":"Product not found"
+                }
+                """)
+                .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "skuMissing")
+    void testMissingSku(MockServer mockServer) throws Exception {
+        URL url = new URL(mockServer.getUrl() + "/catalog/SKU-999");
+        HttpURLConnection connection =
+                (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int statusCode = connection.getResponseCode();
+
+        assertEquals(404, statusCode);
     }
 }
