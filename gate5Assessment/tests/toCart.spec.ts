bb46b619ad2489dfcba1202498b1a2c8086f0d2d
Shahbaz Ahmad(UST <307424@ust.com>
2026-07-04 17:28:40 +0530

    add toCart

---
+++ gate5Assessment/tests/toCart.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "../fixtures/diagnosis";
+
+test('adding product to cart', async ({ page, log }) => {
+    await page.goto('/catalog');
+    await expect(page.getByRole('heading', { name: 'Product Catalog' })).toBeVisible();
+    log.info('Heading visible');
+
+    const search = page.getByRole('form', { name: 'Product filters' });
+
+    await search.getByLabel('Search products').fill('travel');
+    await page.getByRole('button', { name: 'Search' }).click();
+
+    await expect(page.getByTestId('catalog-result-count')).toContainText('3');
+    log.info('Verify Product is visible ');
+
+
+    await search.getByLabel('Search products').pressSequentially('')
+
+    await page.getByRole('link', { name: 'Travel Backpack' }).click();
+    await expect(page).toHaveURL(/\/product\/travel-backpack/);
+    await expect(page.getByRole('heading', { name: 'Travel Backpack' })).toBeVisible();
+    log.info('verify PDP visible');
+
+
+    await page.getByRole('button', { name: 'Add to cart' }).click();
+    await expect(page).toHaveURL(/\/cart/);
+    await expect(page.getByRole('heading', { name: 'Cart' })).toBeVisible();
+    await expect(page.getByTestId('cart-count')).toHaveText('1');
+    log.info('successfully Added to cart visible');
+
+});
