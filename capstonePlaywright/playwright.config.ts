bc648b4c9b5d62f6519598718a87b3dab0294ad2
Kavyashree-g-ust <304556@ust.com>
2026-07-18 12:28:05 +0530

    ci changed

--- capstonePlaywright/playwright.config.ts
+++ capstonePlaywright/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
    use: {
-    headless:false,
+    headless:true,
     screenshot:'only-on-failure',
     video:'retain-on-failure',
     trace: 'retain-on-failure',
