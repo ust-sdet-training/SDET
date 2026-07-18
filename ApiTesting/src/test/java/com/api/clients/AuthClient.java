9607bbc8dd404c83196fc0ab628c23b04ad9da51
Cipiraj Rangaraj Parimaladevi(UST <310971@ust.com>
2026-07-18 23:22:54 +0530

    updated ci for env

--- ApiTesting/src/test/java/com/api/clients/AuthClient.java
+++ ApiTesting/src/test/java/com/api/clients/AuthClient.java
@@ -1,6 +1,7 @@
 package com.api.clients;
 
 import com.api.data.Secrets;
+import com.api.data.TestData;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
@@ -20,7 +21,7 @@ public class AuthClient {
 
     public Response loginAsDave(){
         Map<String, String> body = Map.of(
-                "email", Secrets.get("HEIDI_EMAIL"),
+                "email", TestData.getName()+"@tripstack.test",
                 "password", Secrets.get("HEIDI_PASSWORD")
         );
         return loginSpec(body);
---
+++ ApiTesting/src/test/java/com/api/data/TestData.java
@@ -0,0 +1,13 @@
+package com.api.data;
+
+public class TestData {
+
+
+    public static  String getName() {
+        return "heidi";
+    }
+
+
+
+
+}
