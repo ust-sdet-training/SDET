efb42d2bf658c9b15cbc491a880c517f4f0f8efe
Bhumika Siripireddy(UST <272816@ust.com>
2026-07-19 00:34:21 +0530

    added changes

--- Bhumika_Capstone/OWNDB/databse/DatabaseConfig.ts
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2/promise";
-export async function getConnection() {
-    return await mysql.createConnection({   
-        host: "localhost",
-        user: "root",
-        password: "root@123",
-        database: "tripstack"
-    });
-}
--- Bhumika_Capstone/OWNDB/databse/db.ts
+++ Bhumika_Capstone/OWNDB/databse/db.ts
@@ -1,17 +1,11 @@
 import mysql from "mysql2/promise";
 
 export async function getConnection() {
-
     return mysql.createConnection({
-
-        host: "localhost",
-
-        user: "root",
-
-        password: "root@123",
-
-        database: "tripstack"
-
+        host: process.env.DB_HOST || "localhost",
+        port: Number(process.env.DB_PORT || 3306),
+        user: process.env.DB_USERNAME || "root",
+        password: process.env.DB_PASSWORD || "root@123",
+        database: process.env.DB_NAME || "tripstack"
     });
-
 }
